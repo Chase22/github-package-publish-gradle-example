b42574050750185ba18b8349f421dafd96fdd736
Chase <chase@mailbox.org>
2022-04-27 10:42:32 +0200

    add basic library

---
+++ mathLibrary/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.20"
+    `maven-publish`
+}
+
+group = "de.chasenet.math"
+version = "1.0-SNAPSHOT"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ mathLibrary/src/main/kotlin/Math.kt
@@ -0,0 +1,5 @@
+package de.chasenet.math
+
+object Math {
+    fun add(a: Int, b: Int): Int = a + b
+}
